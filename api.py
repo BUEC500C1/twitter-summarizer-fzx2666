971f1debff4d320b007ddcd3f7f094790f18fff0
fzx2666 <46859529+fzx2666@users.noreply.github.com>
2020-02-01 02:59:12 -0400

    Add files via upload

--- api.py
+++ api.py
@@ -8,10 +8,11 @@ def jprint(obj):
     print(data)
 
 def req():
+    air1 = input("Departure: ")
+    air2 = input("Arrival: ")
     csvFilePath = open('airport_codes.csv', encoding='utf-8', errors='ignore')
-    fieldnames = (
-    "ident", "type", "name", "elevation", "continent", "iso_country", "iso_region", "municipal", "gps_code",
-    "iata_code", "local_code", "coordinates")
+    fieldnames = ("ident", "type", "name", "elevation", "continent", "iso_country", "iso_region", "municipal", "gps_code",
+                  "iata_code", "local_code", "coordinates")
     reader = csv.DictReader(csvFilePath, fieldnames, restval=None, dialect='excel')
     coordinates,name,continent,country = [],[],[],[]
     for row in reader:
@@ -19,15 +20,32 @@ def req():
         name.append(row['name'])
         continent.append(row['continent'])
         country.append(row['iso_country'])
-    api_points = "https://api.weather.gov/points/"+coordinates[2]
-    response = requests.get(api_points)
-    response = response.json()
-    if
-    print(response["properties"]["forecast"])
-    forecast_info = requests.get(response["properties"]["forecast"])
-    forecast_info = forecast_info.json()
-    jprint(forecast_info)
 
+    search_coor_dep = coordinates[name.index(air1)]
+    search_coor_arr = coordinates[name.index(air1)]
+    api_points_dep = "https://api.weather.gov/points/"+search_coor_dep
+    api_points_arr = "https://api.weather.gov/points/"+search_coor_arr
+    response_dep = requests.get(api_points_dep)
+    response_arr = requests.get(api_points_arr)
+    response_dep = response_dep.json()
+    response_arr = response_arr.json()
+    if "properties" not in response_dep: print('Your departure airport is not found.')
+    elif "properties" not in response_arr: print('Your arrival airport is not found.')
+    else:
+        forecast_info_dep = requests.get(response_dep["properties"]["forecast"])
+        forecast_info_dep = forecast_info_dep.json()
+        forecast_info_arr = requests.get(response_arr["properties"]["forecast"])
+        forecast_info_arr = forecast_info_arr.json()
+    print('What information are you looking for? Please Enter following selections:')
+    detail = input('temperature windSpeed windDirection shortForecast detailedForcast\n')
+    print('The information of departure airport')
+    for i in range(13):
+        print(forecast_info_dep['properties']['periods'][i]['name']+": ", end = '')
+        print(forecast_info_dep['properties']['periods'][i][detail])
+    print('\nThe information of arrival airport')
+    for i in range(13):
+        print(forecast_info_dep['properties']['periods'][i]['name'] + ": ", end='')
+        print(forecast_info_dep['properties']['periods'][i][detail])
 
 if __name__ == '__main__':
     req()
